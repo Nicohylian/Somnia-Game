1489886784f455e207a7329d61d9c36b872bde8c
unknown <nicolas.fernandez.1@ug.uchile.cl>
2025-04-22 00:51:36 -0400

    feat: se agrego un personaje, uma luz, sombra y mivel basico

---
+++ proyecto-cc5408/scene/player.gd
@@ -0,0 +1,45 @@
+class_name Player
+extends CharacterBody2D
+
+@onready var shadow_direction: RayCast2D = %ShadowDirection
+@export var direction: Vector2 = Vector2(1,0)
+@onready var shadow: Shadow = %Shadow
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+
+
+
+
+func _physics_process(delta: float) -> void:
+	
+	shadow_direction.rotation = direction.angle()
+	
+	
+	
+	if shadow_direction.collide_with_bodies:
+		shadow.visible = true
+		shadow.global_position = shadow_direction.get_collision_point()
+	else:
+		shadow.hide()
+		shadow.global_position = direction
+		
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
