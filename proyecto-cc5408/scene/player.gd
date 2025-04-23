0e33e2cda15fcbb35e844b324a1f355561eee604
unknown <nicolas.fernandez.1@ug.uchile.cl>
2025-04-23 02:15:59 -0400

    feat: el personaje se puede teletransportar a su sombra

--- proyecto-cc5408/scene/player.gd
+++ proyecto-cc5408/scene/player.gd
@@ -15,15 +15,9 @@ const JUMP_VELOCITY = -400.0
 
 
 
-
 func _physics_process(delta: float) -> void:
 	
 	shadow_direction.rotation = direction.angle()
-	
-	
-	
-	
-	
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -32,6 +26,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("action"):
+		if shadow_direction.is_colliding():
+			global_position = shadow.global_position
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
