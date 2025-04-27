ca3c564f64c08cba7f4997de4980f0c19fcee2fc
unknown <nicolas.fernandez.1@ug.uchile.cl>
2025-04-26 23:15:09 -0400

    feat: se agrego algunas funcionalidades

--- proyecto-cc5408/scene/main_menu.gd
+++ proyecto-cc5408/scene/main_menu.gd
@@ -1,9 +1,13 @@
 extends MarginContainer
 
+@onready var start: Button = %Start
+@onready var credits: Button = %Credits
+@onready var quit: Button = %Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start.pressed.connect(func(): get_tree().change_scene_to_file("res://scene/level_1.tscn"))
+	quit.pressed.connect(func(): get_tree().quit())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
