4516560154b4de3c28e7160b611a32f47ac67a82
unknown <nicolas.fernandez.1@ug.uchile.cl>
2025-04-26 21:37:00 -0400

    feat: ahora se calcula la sombra principal a la luz mas cercana

--- proyecto-cc5408/scene/level_1.gd
+++ proyecto-cc5408/scene/level_1.gd
@@ -4,14 +4,23 @@ extends Node2D
 @onready var player: Player = %Player
 @onready var light: Node2D = %Light
 
+var lights: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	lights = get_tree().get_nodes_in_group("Light")
 	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var shadow_direction: Vector2 = player.global_position - light.global_position
+	var current_light: Light = null
+	for light in lights:
+		if current_light == null:
+			current_light = light
+		else:
+			if (player.global_position - light.global_position).length() < (player.global_position - current_light.global_position).length():
+				current_light = light
+		
+	var shadow_direction: Vector2 = player.global_position - current_light.global_position
 	player.direction1 = shadow_direction
